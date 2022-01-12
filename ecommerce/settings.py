b48c855a22915e86d168fc0d7d9a60668c07ab6a
unknown <hasanovtural11@gmail.com>
2022-01-12 14:52:18 +0400

    static 1

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -134,10 +134,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'media'
 ]
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 STATIC_ROOT = BASE_DIR / 'static_cdn'
-MEDIA_ROOT = BASE_DIR / 'media_cdn'
