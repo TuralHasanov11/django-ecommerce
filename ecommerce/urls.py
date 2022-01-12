843aaac64ed354dc341cbd1b401690c40252a792
unknown <hasanovtural11@gmail.com>
2022-01-12 15:15:48 +0400

    media 6

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', include('store.urls')),
 
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
